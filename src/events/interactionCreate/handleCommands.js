c5dfe3fd49ddcc876cc67e82ed4c10a92e980c41
イム様 <113910380+Imu-D-sama@users.noreply.github.com>
2025-01-01 04:38:16 +0300

    added the command handler so that commands work

---
+++ src/events/interactionCreate/handleCommands.js
@@ -0,0 +1,99 @@
+const { Client, CommandInteraction } = require("discord.js");
+const { devs, MainServer } = require("../../../config.json");
+const { HighStaff } = require("../../../config.json");
+const getLocalCommands = require("../../utils/getLocalCommands");
+
+/**
+ *
+ * @param {Client} client
+ * @param {CommandInteraction} interaction
+ * @returns
+ */
+module.exports = async (client, interaction) => {
+  if (!interaction.isChatInputCommand() && !interaction.isContextMenuCommand())
+    return;
+
+  const localCommands = getLocalCommands();
+  const OWNERID = devs[0];
+
+  try {
+    const commandObject = localCommands.find(
+      (cmd) => cmd.name === interaction.commandName
+    );
+
+    if (!commandObject) return;
+
+    if (commandObject.devOnly) {
+      if (
+        !devs.includes(interaction.member.id) &&
+        interaction.member.id !== OWNERID
+      ) {
+        interaction.reply({
+          content: "❌Only developers are allowed to run this command.",
+          ephemeral: true,
+        });
+        return;
+      }
+    }
+
+    if (commandObject.MainOnly) {
+      if (!(interaction.guild.id === MainServer)) {
+        interaction.reply({
+          content: "❌This Command Can't Run here.",
+          ephemeral: true,
+        });
+        return;
+      }
+    }
+
+    if (interaction.member && commandObject.permissionsRequired?.length) {
+      for (const permission of commandObject.permissionsRequired) {
+        if (
+          !interaction.member.permissions.has(permission) &&
+          interaction.member.id !== OWNERID &&
+          !interaction.member.roles.cache.has(HighStaff)
+        ) {
+          interaction.reply({
+            content: "Not enough permissions.",
+            ephemeral: true,
+          });
+          return;
+        }
+      }
+    }
+    if (interaction.member && commandObject.Roles?.length) {
+      const hasRequiredRole = commandObject.Roles.some((role) =>
+        interaction.member.roles.cache.has(role)
+      );
+
+      const isOwner = interaction.member.id === OWNERID;
+      const isHighStaff = interaction.member.roles.cache.has(HighStaff);
+
+      if (!hasRequiredRole && !isOwner && !isHighStaff) {
+        interaction.reply({
+          content: "Not enough permissions.",
+          ephemeral: true,
+        });
+        return;
+      }
+    }
+
+    if (commandObject.botPermissions?.length) {
+      for (const permission of commandObject.botPermissions) {
+        const bot = interaction.guild.members.me;
+
+        if (!bot.permissions.has(permission)) {
+          interaction.reply({
+            content: "I don't have enough permissions to execute this command.",
+            ephemeral: true,
+          });
+          return; // Add this line to exit the function and prevent further execution
+        }
+      }
+    }
+
+    await commandObject.callback(client, interaction);
+  } catch (error) {
+    console.log(`There was an error running this command in handle: ${error}`);
+  }
+};

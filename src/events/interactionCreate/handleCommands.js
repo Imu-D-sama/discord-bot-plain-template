0a4ba244c6b1e161192eaf4e37abd4b8d148e4a4
イム様 <113910380+Imu-D-sama@users.noreply.github.com>
2025-01-19 10:47:29 +0300

    main dev should skip all checks(slashcommands)

--- src/events/interactionCreate/handleCommands.js
+++ src/events/interactionCreate/handleCommands.js
@@ -14,6 +14,7 @@ module.exports = async (client, interaction) => {
 
   const localCommands = getLocalCommands();
   const OWNERID = devs[0];
+  const isOwner = interaction.member.id === OWNERID;
 
   try {
     const commandObject = localCommands.find(
@@ -21,36 +22,44 @@ module.exports = async (client, interaction) => {
     );
 
     if (!commandObject) return;
-
-    if (commandObject.devOnly) {
-      if (
-        !devs.includes(interaction.member.id) &&
-        interaction.member.id !== OWNERID
-      ) {
-        interaction.reply({
-          content: "❌Only developers are allowed to run this command.",
-          ephemeral: true,
-        });
-        return;
+    if (interaction.user.id !== OWNERID) {
+      if (commandObject.devOnly) {
+        if (!devs.includes(interaction.member.id)) {
+          interaction.reply({
+            content: "❌Only developers are allowed to run this command.",
+            ephemeral: true,
+          });
+          return;
+        }
       }
-    }
 
-    if (commandObject.MainOnly) {
-      if (!(interaction.guild.id === MainServer)) {
-        interaction.reply({
-          content: "❌This Command Can't Run here.",
-          ephemeral: true,
-        });
-        return;
+      if (commandObject.MainOnly) {
+        if (!(interaction.guild.id === MainServer)) {
+          interaction.reply({
+            content: "❌This Command Can't Run here.",
+            ephemeral: true,
+          });
+          return;
+        }
       }
-    }
 
-    if (interaction.member && commandObject.permissionsRequired?.length) {
-      for (const permission of commandObject.permissionsRequired) {
-        if (
-          !interaction.member.permissions.has(permission) &&
-          interaction.member.id !== OWNERID
-        ) {
+      if (interaction.member && commandObject.permissionsRequired?.length) {
+        for (const permission of commandObject.permissionsRequired) {
+          if (!interaction.member.permissions.has(permission)) {
+            interaction.reply({
+              content: "Not enough permissions.",
+              ephemeral: true,
+            });
+            return;
+          }
+        }
+      }
+      if (interaction.member && commandObject.Roles?.length) {
+        const hasRequiredRole = commandObject.Roles.some((role) =>
+          interaction.member.roles.cache.has(role)
+        );
+
+        if (!hasRequiredRole && !isOwner) {
           interaction.reply({
             content: "Not enough permissions.",
             ephemeral: true,
@@ -58,33 +67,19 @@ module.exports = async (client, interaction) => {
           return;
         }
       }
-    }
-    if (interaction.member && commandObject.Roles?.length) {
-      const hasRequiredRole = commandObject.Roles.some((role) =>
-        interaction.member.roles.cache.has(role)
-      );
 
-      const isOwner = interaction.member.id === OWNERID;
+      if (commandObject.botPermissions?.length) {
+        for (const permission of commandObject.botPermissions) {
+          const bot = interaction.guild.members.me;
 
-      if (!hasRequiredRole && !isOwner) {
-        interaction.reply({
-          content: "Not enough permissions.",
-          ephemeral: true,
-        });
-        return;
-      }
-    }
-
-    if (commandObject.botPermissions?.length) {
-      for (const permission of commandObject.botPermissions) {
-        const bot = interaction.guild.members.me;
-
-        if (!bot.permissions.has(permission)) {
-          interaction.reply({
-            content: "I don't have enough permissions to execute this command.",
-            ephemeral: true,
-          });
-          return; // Add this line to exit the function and prevent further execution
+          if (!bot.permissions.has(permission)) {
+            interaction.reply({
+              content:
+                "I don't have enough permissions to execute this command.",
+              ephemeral: true,
+            });
+            return;
+          }
         }
       }
     }
